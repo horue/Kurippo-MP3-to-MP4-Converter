389cd7973d6b90cf6f1e9d02ff18e9962e2c635e
Jorge <jorgejapao00@gmail.com>
2024-04-20 18:28:47 -0300

    Button to start converting the files added

--- GUI/gui.py
+++ GUI/gui.py
@@ -6,6 +6,9 @@ import moviepy.editor as mp
 
 
 
+def run():
+    pass
+
 def initial(root):
     t1 = tk.Label(root, text="MP3 to MP4 Converter")
     t1.pack()
@@ -15,6 +18,9 @@ def initial(root):
 
     b2 = tk.Button(root, text='Select Sound',command=lambda:filedialog.askopenfile())
     b2.pack()
+
+    b3 = tk.Button(root, text='Convert', command=lambda:run(b1, b2))
+    b3.pack
 
 
 
