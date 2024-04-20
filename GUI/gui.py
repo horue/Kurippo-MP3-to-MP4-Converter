b96f5acb63a86d73d29fdead7829d31fe7fba923
Jorge <jorgejapao00@gmail.com>
2024-04-20 18:50:44 -0300

    Convert function added

--- GUI/gui.py
+++ GUI/gui.py
@@ -5,10 +5,24 @@ import os
 import moviepy.editor as mp
 
 
-def run():
+def run(b1, b2):
     outputPath = os.path.join(os.path.expanduser("~"), "Documents\\MP3 to MP4 Converter\\Converted Videos")
     if not os.path.exists(outputPath):
         os.makedirs(outputPath) 
+
+    audio_path = b1.read
+    image_path = b2.read
+    audio = mp.AudioFileClip(audio_path, fps=44100)
+    video = mp.VideoFileClip(image_path)
+
+
+    video_duration = audio.duration
+
+
+    final_video = video.set_audio(audio).set_duration(video_duration)
+
+
+    final_video.write_videofile(outputPath, fps=60) 
 
 
 
@@ -25,8 +39,8 @@ def initial(root):
     b2 = tk.Button(root, text='Select Sound',command=lambda:filedialog.askopenfile())
     b2.pack()
 
-    b3 = tk.Button(root, text='Convert', command=lambda:run(b1, b2))
-    b3.pack
+    b3 = tk.Button(root, text='Convert',command=lambda:run(b1, b2))
+    b3.pack()
 
 
 
