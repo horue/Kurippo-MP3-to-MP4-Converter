07225acca0d5d2975b8efe05514282723215138b
Jorge Magno <jorgejapao00@gmail.com>
2024-04-24 16:03:33 -0300

    CTKinter intarface started

--- GUI/gui2.py
+++ GUI/gui2.py
@@ -1,1 +1,14 @@
 import customtkinter as ct
+
+
+def main():
+    root = ct.CTk()
+    root.geometry("600x500")
+    root.title("MP3 to MP4 Converter")
+
+    
+    root.iconify()
+    root.update()
+    root.deiconify()
+    root.mainloop()
+main()
