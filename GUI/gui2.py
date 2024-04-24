4f4bae66aad01fc504639c1745ee0859fb679c05
Jorge Magno <jorgejapao00@gmail.com>
2024-04-24 16:06:20 -0300

    Function added to CTkinter GUI

--- GUI/gui2.py
+++ GUI/gui2.py
@@ -1,4 +1,60 @@
 import customtkinter as ct
+import easygui as eg
+import PIL
+import os 
+import moviepy.editor as mp
+
+
+
+
+
+
+
+def open_image():
+    global image
+    image = eg.fileopenbox()
+
+def open_sound():
+    global sound
+    sound = eg.fileopenbox()
+
+
+def run():
+    nome = str(os.path.basename(sound))
+    name = os.path.splitext(nome)[0]
+
+    outputPath = os.path.join(os.path.expanduser("~"), fr"Documents\{name}.mp4")
+
+    audio = mp.AudioFileClip(str(sound), fps=44100)
+    video = mp.VideoFileClip(str(image))
+
+
+    video_duration = audio.duration
+
+
+    final_video = video.set_audio(audio).set_duration(video_duration)
+
+
+    final_video.write_videofile(outputPath, fps=60) 
+
+
+
+
+def initial(root):
+    t1 = ct.CTkLabel(root, text="MP3 to MP4 Converter")
+    t1.pack()
+
+    b1 = ct.CTkButton(root, text='Select Image',command=open_image)
+    b1.pack()
+
+    b2 = ct.CTkButton(root, text='Select Sound',command=open_sound)
+    b2.pack()
+
+    b3 = ct.CTkButton(root, text='Convert',command=lambda:run())
+    b3.pack()
+
+
+
 
 
 def main():
@@ -6,6 +62,7 @@ def main():
     root.geometry("600x500")
     root.title("MP3 to MP4 Converter")
 
+    initial(root)
     
     root.iconify()
     root.update()
