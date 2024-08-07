29b0296347a9de4d3859dedc181fec875fc46a5d
Jorge <jorgejapao00@gmail.com>
2024-08-06 22:41:42 -0300

    Function to select path and save created. Dropdown menu updated with default themes.

--- GUI/preferences.py
+++ GUI/preferences.py
@@ -1,10 +1,24 @@
 import customtkinter as ct
+from customtkinter import filedialog
 import sys
 import os
 from read_krip import *
+from save_krip import *
 
+    
 
 class toplevel():
+
+    def save(path, theme):
+        save_path(p=path)
+        save_theme(t=theme)
+        
+    def select_path():
+        global np
+        np = filedialog.askdirectory()
+
+
+
     def preferences(title='Edit preferences', size='500x400'):
         tl = ct.CTkToplevel()
         tl.geometry(size)
@@ -16,18 +30,21 @@ class toplevel():
         l1 = ct.CTkLabel(tl, text=f'Conversion destiny: {get_path()}')
         l1.pack()
 
-        b1 = ct.CTkButton(tl, text='Edit')
+        b1 = ct.CTkButton(tl, text='Edit', command=lambda:toplevel.select_path())
         b1.pack()
 
         
         l2 = ct.CTkLabel(tl, text=f'Theme: {get_theme()}')
         l2.pack()
 
-        b2 = ct.CTkButton(tl, text='Edit')
-        b2.pack()
+        c1 = ct.CTkComboBox(tl, values=["Light", "Dark"])
+        c1.pack()
 
-        b3 = ct.CTkButton(tl, text='Save')
+        b3 = ct.CTkButton(tl, text='Save', command=lambda:toplevel.save(path=np, theme=c1.get()))
         b3.pack()
+
+        b4 = ct.CTkButton(tl, text='Apply')
+        b4.pack()
 
 
         for child in tl.winfo_children():
